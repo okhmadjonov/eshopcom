dae1ea853ae08ac3318d62abbd5bc6b31f15e6c4
okhmadjonov <oakhmadjonov1@gmail.com>
2021-02-04 22:26:49 +0500

    Added React router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import { Switch, Route } from "react-router-dom";
 
+export const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -9,32 +9,32 @@ class Directory extends React.Component {
       sections: [
         {
           title: "Thrillers",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl: "https://i.ibb.co/8NXxrL5/portfolio3.jpg",
           id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "hats",
         },
         {
           title: "Romance",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl: "https://i.ibb.co/BqhpVDq/03.jpg",
           id: 2,
           linkUrl: "shop/jackets",
         },
         {
           title: "Mysteries",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imageUrl: "https://i.ibb.co/TLdxMCk/portfolio4.jpg",
           id: 3,
           linkUrl: "shop/sneakers",
         },
         {
           title: "Science Fiction",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imageUrl: "https://i.ibb.co/XkCsHJR/01-1.jpg",
           size: "large",
           id: 4,
           linkUrl: "shop/womens",
         },
         {
           title: "Children",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl: "https://i.ibb.co/QXQTN5M/02.jpg",
           size: "large",
           id: 5,
           linkUrl: "shop/mens",
@@ -46,8 +46,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSecionProps }) => (
+          <MenuItem key={id} {...otherSecionProps} />
         ))}
       </div>
     );
