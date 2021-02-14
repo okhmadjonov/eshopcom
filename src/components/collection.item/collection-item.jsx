a44eddfa189c46e97f99ec2a9e53f7facbef32ac
okhmadjonov <oakhmadjonov1@gmail.com>
2021-02-14 09:56:47 +0500

    aded some desing for homepage

--- src/components/collection.item/collection-item.jsx
+++ src/components/collection.item/collection-item.jsx
@@ -3,12 +3,10 @@ import "./collection-item.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
-    <div className="image" 
-    style={{ backgroundImage: `url(${imageUrl})` }}
-     />
+    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="collection-footer">
       <span className="name">{name}</span>
-      <span className="price">{price}</span>
+      <span className="price">{price}$</span>
     </div>
   </div>
 );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,38 +7,31 @@ import { auth } from "../../firebase/firebase.utils";
 const Header = ({ currentUser }) => {
   return (
     <div className="header">
-      <Link className="logo-container" to="/" style={{ color: "#926" }}>
-        e.SHOP
-      </Link>
-      <div className="options">
-        <Link className="option" to="/shop">
-          Shop
+      <div className="container">
+        <Link className="logo-container" to="/">
+          Clothes House
         </Link>
-        <Link className="option" to="/contact">
-          Contact
-        </Link>
-
-        {currentUser ? (
-          <div className="option" onClick={() => auth.signOut()}>
-            Sign out
-          </div>
-        ) : (
-          <Link className="option" to="/signin">
-            Sign in
+        <div className="options">
+          <Link className="option" to="/shop">
+            Shop
           </Link>
-        )}
-        <Link
-          className="option"
-          to="/bag"
-          style={{
-            fontSize: "1.2em",
-            color: "#acadac",
-            opacity: "0.8",
-            fontWeight: "350",
-          }}
-        >
-          <i className="fa fa-shopping-cart"></i>
-        </Link>
+          <Link className="option" to="/contact">
+            Contact
+          </Link>
+
+          {currentUser ? (
+            <div className="option" onClick={() => auth.signOut()}>
+              Sign out
+            </div>
+          ) : (
+            <Link className="option" to="/signin">
+              Sign in
+            </Link>
+          )}
+          <Link className="option" to="/bag">
+            <i className="fa fa-shopping-cart"></i>
+          </Link>
+        </div>
       </div>
     </div>
   );
